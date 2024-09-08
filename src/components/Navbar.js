2fc3879017d4347efcadbe9279a9c3567b30eafd
MuhammadHassan <muhammadhaxxan6@gmail.com>
2024-09-08 14:51:12 +0500

    Working Frontend

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import './Navbar.css'; 
+import './Navbar.css';
 
 const Navbar = () => {
     const [isLoggedIn, setIsLoggedIn] = React.useState(!!localStorage.getItem('authToken')); // Check if the user is logged in
     const navigate = useNavigate();
 
     const handleLogout = () => {
-       
+
         localStorage.removeItem('authToken'); // Remove the token from localStorage
         setIsLoggedIn(false);
         navigate('/login'); // Redirect to login page after logout
@@ -18,7 +18,7 @@ const Navbar = () => {
             <div className="navbar-links">
                 <Link to="/productlist" className="navbar-link">Home</Link>
                 <Link to="/productform" className="navbar-link">Add Product</Link>
-                <Link to="/productform/:id" className="navbar-link">Add Product</Link>
+                <Link to="/productform/:id" className="navbar-link">Product</Link>
             </div>
             <div className="navbar-profile">
                 <Link to="/profile" className="navbar-link">Profile</Link>
